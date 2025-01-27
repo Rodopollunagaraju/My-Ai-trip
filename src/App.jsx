b8f451b02e1cfc663db109722f68975965e1d95e
Rodopollunagaraju <rodopollunagaraju@gmail.com>
2025-01-27 17:50:09 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Button } from './components/ui/button'
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import Header from './components/costom/Header'
 import Create from './components/costom/Create-trip'
-import Index from './components/view/[tripid]'
+import Index from './components/view/Tripid/Index'
 import Mytrip from './components/costom/Mytrip'
 function App() {
   return (
